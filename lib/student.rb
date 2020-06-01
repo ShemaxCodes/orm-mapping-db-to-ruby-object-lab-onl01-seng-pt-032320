c98018681d70c5c3b08a1db4a0260cac1d14220d
Jashema Panapa <jcpanapa@gmail.com>
2020-06-01 02:37:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
     # remember each row should be a new instance of the Student class
   end
 
-    def self.find_by_name(name)
+  def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
       FROM students
@@ -24,7 +24,7 @@ class Student
  
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end.first
+  end.first
   end
   
   def save
